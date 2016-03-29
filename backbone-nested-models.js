d6c5bd44a23899f14694b09d93e662d57dc09c19
Ted Johnson <johnsontedm@gmail.com>
2016-03-28 22:36:52 -0500

    Update backbone-nested-models.js
    Initial re-write of the set function.

--- backbone-nested-models.js
+++ backbone-nested-models.js
@@ -24,7 +24,8 @@
 }(this, function (Backbone, _) {
 
     var Model = Backbone.Model,
-        Collection = Backbone.Collection;
+        Collection = Backbone.Collection,
+        originalSet = Backbone.Model.prototype.set;
 
     Backbone.Model.prototype.setRelation = function(attr, val, options) {
         var relation = this.attributes[attr],
@@ -77,72 +78,11 @@
     };
 
     Backbone.Model.prototype.set = function(key, val, options) {
-        var attr, attrs, unset, changes, silent, changing, prev, current;
-        if (key == null) return this;
-
-        // Handle both `"key", value` and `{key: value}` -style arguments.
-        if (typeof key === 'object') {
-            attrs = key;
-            options = val;
-        } else {
-            (attrs = {})[key] = val;
-        }
-
-        options || (options = {});
-
-        // Run validation.
-        if (!this._validate(attrs, options)) return false;
-
-        // Extract attributes and options.
-        unset           = options.unset;
-        silent          = options.silent;
-        changes         = [];
-        changing        = this._changing;
-        this._changing  = true;
-
-        if (!changing) {
-            this._previousAttributes = _.clone(this.attributes);
-            this.changed = {};
-        }
-        current = this.attributes, prev = this._previousAttributes;
-
-        // Check for changes of `id`.
-        if (this.idAttribute in attrs) this.id = attrs[this.idAttribute];
-
-        // For each `set` attribute, update or delete the current value.
-        for (attr in attrs) {
-            val = attrs[attr];
-
-            // Inject in the relational lookup
-            val = this.setRelation(attr, val, options);
-
-            if (!_.isEqual(current[attr], val)) changes.push(attr);
-            if (!_.isEqual(prev[attr], val)) {
-                this.changed[attr] = val;
-            } else {
-                delete this.changed[attr];
-            }
-            unset ? delete current[attr] : current[attr] = val;
-        }
-
-        // Trigger all relevant attribute changes.
-        if (!silent) {
-            if (changes.length) this._pending = true;
-            for (var i = 0, l = changes.length; i < l; i++) {
-                this.trigger('change:' + changes[i], this, current[changes[i]], options);
-            }
-        }
-
-        if (changing) return this;
-        if (!silent) {
-            while (this._pending) {
-                this._pending = false;
-                this.trigger('change', this, options);
-            }
-        }
-        this._pending = false;
-        this._changing = false;
-        return this;
+        //Calling setRelation, not using the return value.
+        this.setRelation.apply(this, arguements);
+        
+        //Calling the original set.
+        return originalSet.apply(this, arguements);
     };
 
     Backbone.Model.prototype.toJSON = function(options) {
